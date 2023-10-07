bbc2765712e342cc952aff01fd249c708fd47a4c
YaroslavTsvirkun <yaroslav.tsvirkun.1992@outlook.com>
2023-10-07 15:31:49 +0300

    Update myApp.js
    change myApp.js, implemented findAndUpdate function

--- myApp.js
+++ myApp.js
@@ -75,7 +75,12 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  PersonModel.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true },
+    function(err, updatedDoc) {
+      if (err) return console.log(err);
+      done(null, updatedDoc);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
